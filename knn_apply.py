1be9c342f2fc1214bbf9786d5727f1a35e8e6195
wKoja <cfwilliam.ferreira@gmail.com>
2021-01-22 19:57:46 -0300

    implementation of the K-NN algorithm

---
+++ knn_apply.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn import preprocessing, model_selection, neighbors
+import pandas as pd
+
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+
+# features
+X = np.array(df.drop(['class'], 1))
+# label / class
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    X, y, test_size=0.2)
+
+
+# classfier
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
+
+example_measures = np.array(
+    [[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 1, 2, 2, 2, 3, 2, 1]])
+example_measures = example_measures.reshape(len(example_measures), -1)
+
+prediction = clf.predict(example_measures)
+
+print(prediction)
